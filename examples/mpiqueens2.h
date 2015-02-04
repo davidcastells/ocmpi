3bb5299476a5a7ff1e9009045df815ee635ec15b
dcr <dcr@158.109.70.33>
2015-02-04 19:48:24 +0100

    (c)

--- examples/mpiqueens2.h
+++ examples/mpiqueens2.h
@@ -1,8 +1,18 @@
-/*
- * mpiqueens2.h
+/**
+ * 
+ * Copyright (C) 2015, David Castells-Rufas <david.castells@uab.es>, CEPHIS, Universitat Autonoma de Barcelona  
  *
- *  Created on: 17/12/2014
- *      Author: test64
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef MPIQUEENS2_H_

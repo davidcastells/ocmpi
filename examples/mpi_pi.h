a4c894b8254641b53dbc99d0ef859c044282b7dc
dcr <dcr@158.109.70.33>
2015-02-04 17:52:32 +0100

    PI computing example

---
+++ examples/mpi_pi.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   mpi_pi.h
+ * Author: dcr
+ *
+ * Created on 4 de febrero de 2015, 17:06
+ */
+
+#ifndef MPI_PI_H
+#define	MPI_PI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+int mpi_pi_main(int argc, char *argv[]) ;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MPI_PI_H */
+

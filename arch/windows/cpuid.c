dd8b3b5feb52813aa174989f74304029a6e63225
dcr <dcr@158.109.70.33>
2015-02-04 19:49:47 +0100

    (c)

--- arch/windows/cpuid.c
+++ arch/windows/cpuid.c
@@ -1,3 +1,19 @@
+/**
+ * 
+ * Copyright (C) 2015, David Castells-Rufas <david.castells@uab.es>, CEPHIS, Universitat Autonoma de Barcelona  
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <windows.h>
 #include "cpuid.h"
 #include "../../config.h"
@@ -35,7 +51,7 @@ void createSlaveThreads(main_func mf)
 
         mpi_threads[i] = tid;
 
-        printf("Slave %d has handle %p\n", i, mpi_threads[i]);
+        //printf("Slave %d has handle %p\n", i, mpi_threads[i]);
     }
     
 }
@@ -45,7 +61,7 @@ DWORD ThreadProc (LPVOID lpdwThreadParam )
 {
     main_func mf = (main_func) lpdwThreadParam;
     
-    printf("Going to invoke main from thread %p\n", GetCurrentThreadId());
+    //printf("Going to invoke main from thread %p\n", GetCurrentThreadId());
     
     (*mf)(0, NULL);
 }

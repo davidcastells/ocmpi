c9e4c957332f369330cb6576b3cc0473b07ea2e1
David Castells-Rufas <david.castells@uab.es>
2018-02-27 16:51:01 +0100

    license

--- config.h
+++ config.h
@@ -1,3 +1,28 @@
+/**
+ * Copyright (C) David Castells-Rufas, CEPHIS, Universitat Autonoma de Barcelona  
+ * david.castells@uab.cat
+ * 
+ * This work was used in the publication of 
+ * "128-core Many-Soft-Core Processor with MPI support" 
+ * available online on 
+ * https://www.researchgate.net/publication/282124163_128-core_Many-Soft-Core_Processor_with_MPI_support
+ * 
+ * I encourage that you cite it as:
+ * [*] Castells-Rufas, David, and Jordi Carrabina. "128-core Many-Soft-Core Processor with MPI support." 
+ * Jornadas de Computación Reconfigurable y Aplicaciones (JCRA) (2015).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * config.h
  *
